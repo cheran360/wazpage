1456b343f736da9faedd6d56882956b8333d8d1b
cheran <cherankumar.548@gmail.com>
2022-05-06 17:12:29 +0530

    simpler code

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,37 @@
+// import request
+const request = require("request");
+const beautify = require("json-beautify");
+// express
 const express = require("express");
+const app = express();
 
-app = express();
-const PORT = 5000;
+// body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// on get request
+app.get("/api/getData", (req, res) => {
+  apiUrl = "https://api.wazirx.com/api/v2/tickers";
+  // get request
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      var data = JSON.parse(body);
+      // only first 10 objects
+      keys = Object.keys(data);
+      keys = keys.slice(10, keys.length);
+      // remove all keys
+      for (var i = 0; i < keys.length; i++) {
+        delete data[keys[i]];
+      }
 
-app.use("/api/getData", require("./routes/getData"));
+      res.status(200).json(data);
+    }
+  });
+});
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+// run server
+app.listen(5000, () => {
+  console.log("Server is running on port 3000");
 });
--- backend/routes/getData.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getData } = require("../controllers/dataController");
-
-router.route("/").get(getData);
-
-module.exports = router;
